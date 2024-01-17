c05d5fdbe0e96c5c3e0835b3f64976e5144e3d31
Matheus Quevedo <114124076+mthquevedo@users.noreply.github.com>
2024-01-17 17:54:03 -0300

    Adição de exemplo javascript

---
+++ 💻 Códigos/Javascript/If para campo de imagem condicional.js
@@ -0,0 +1,9 @@
+if ("[Proposta.Nome]".includes("Item 1")) {
+    'adicione aqui o link da imagem'
+}
+else if ("[Proposta.Nome]".includes("Item 2")) {
+    'adicione aqui o link da imagem'
+}
+else if ("[Proposta.Nome]".includes("Item 3")) {
+    'adicione aqui o link da imagem'
+}
